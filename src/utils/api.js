f658bfb079a17e1f158a6d0811dbeecbcef3abb8
Idin Iskandar <167355867+idincodingweb@users.noreply.github.com>
2025-05-28 02:10:50 +0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = "AIzaSyCMDXmZ8yIwAIUkbJXJ2wlUIzRogwd926g"; // Simpan API key di tempat yang aman (gunakan .env pada pengembangan lebih lanjut)
+const apiKey = "AIzaSyBNbQoZs6Fn-zCXl_BxBKTX6Q_5bf5GeFw"; // Simpan API key di tempat yang aman (gunakan .env pada pengembangan lebih lanjut)
 const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`;
 
 export const sendMessage = async (message) => {
